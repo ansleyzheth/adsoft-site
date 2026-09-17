8c055bdc4cc2ff5398dbc9b81ded6e0e854eb21f
Ansley Gonçalves <ansley_dg@hotmail.com>
2026-09-17 17:30:49 -0300

    Configurar GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/adsoft-site",
+  assetPrefix: "/adsoft-site/",
 };
 
 export default nextConfig;
